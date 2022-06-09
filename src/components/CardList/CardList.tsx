e672a933440982b3b4bf2bd84562374ff21409fc
bergstenarn <andersbergsten@yahoo.se>
2022-06-09 21:24:07 +0300

    195. Typing React's setState

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,7 +1,12 @@
+import { Monster } from "../../App";
 import "./CardList.css";
 import Card from "../Card/Card";
 
-const CardList = ({ monsters }) => (
+type CardListProps = {
+  monsters: Monster[];
+};
+
+const CardList = ({ monsters }: CardListProps) => (
   <div className="card-list">
     {monsters.map((monster) => (
       <Card monster={monster} />
